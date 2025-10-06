6e1c7a521540e6235ed2991cd34ca9223a7ab97d
Nidha Fathima <nidhapa00@gmail.com>
2025-10-06 12:06:25 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['Portfolioapp.onrender.com']
+ALLOWED_HOSTS = ["portfolio-a0er.onrender.com", "localhost", "127.0.0.1"]
 
 
 
