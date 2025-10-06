2522751af3bcf716031adf5ed9d3b139cf078639
Nidha Fathima <nidhapa00@gmail.com>
2025-10-06 11:47:54 +0530

    Updated portfolio image

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5hthldigx5+q07_r$-zqctr2%_55t6@h(c0hf56w@1(!@ji=_n'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['Portfolioapp.onrender.com']
 
 
 
